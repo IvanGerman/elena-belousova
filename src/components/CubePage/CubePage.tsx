0ebad174078763fc0eed1110de971190001769d2
IvanGerman <ivgvi@mail.ru>
2023-02-28 18:15:05 +0700

    feat: add cube to the CubePage

--- src/components/CubePage/CubePage.tsx
+++ src/components/CubePage/CubePage.tsx
@@ -1,17 +1,89 @@
 import React  from 'react';
+import { Swiper, SwiperSlide } from "swiper/react";
 
 import styles from './CubePage.module.scss';
+import './CubePage.css';
+import "swiper/css";
+import "swiper/css/effect-cube";
+import "swiper/css/pagination";
+import "swiper/css/autoplay";
 
+import { EffectCube, Pagination, Autoplay } from "swiper";
+
+import Image1 from './../../assets/images/all-items.jpg';
+import Image2 from './../../assets/images/two-dwarfs.jpg';
+import Image3 from './../../assets/images/red-dwarf1.jpg';
+import Image4 from './../../assets/images/grey-dwarf1.jpg';
+import Image5 from './../../assets/images/red-dwarf2.jpg';
+import Image6 from './../../assets/images/bear1.jpg';
+import Image7 from './../../assets/images/bear2.jpg';
+import Image8 from './../../assets/images/violett-bear.jpg';
+import Image9 from './../../assets/images/red-shoes.jpeg';
+import Image10 from './../../assets/images/blue-shoes.jpeg';
+import Image11 from './../../assets/images/beresta.jpg';
+import Image12 from './../../assets/images/chair.jpg';
+import Image13 from './../../assets/images/pillow1.jpg';
 
 
 const CubePage = () => {
 
-  
-  
-
   return (
     <div className={styles.container}>
-     
+       <Swiper
+        effect={"cube"}
+        grabCursor={true}
+        cubeEffect={{
+          shadow: true,
+          slideShadows: true,
+          shadowOffset: 20,
+          shadowScale: 0.94,
+        }}
+        autoplay={ { delay: 3000 } }
+        pagination={true}
+        modules={[EffectCube, Pagination, Autoplay]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <img src={Image1} className="slideImage"/>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={Image2} className="slideImage"/>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={Image3} className="slideImage"/>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={Image4} className="slideImage"/>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={Image5}  className="slideImage"/>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={Image6}  className="slideImage"/>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={Image7}  className="slideImage"/>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={Image8}  className="slideImage"/>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={Image9}  className="slideImage"/>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={Image10}  className="slideImage"/>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={Image11}  className="slideImage"/>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={Image12}  className="slideImage"/>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={Image13}  className="slideImage"/>
+        </SwiperSlide>
+        
+      </Swiper>
     </div>
   );
 };
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
 import styles from './Header.module.scss';
+import Image1 from './../../assets/images/big-board.jpg';
 
 
 
@@ -12,11 +13,20 @@ const Header = () => {
   return (
     <header className={styles.header}>
       <div className={styles.elenaImgDiv}>
-        <img src='' alt='' className={styles.elenaImg}/>
+        <img src={Image1} alt='' className={styles.elenaImg}/>
       </div>
       <div className={styles.descriptionDiv}>
-        <div className={styles.langOptions}></div>
-        <div className={styles.description}></div>
+        <div className={styles.langOptions}>
+          <div className={styles.langWrapper}>
+            <div className={styles.ruslang}></div>
+            <div className={styles.englang}></div>
+            <div className={styles.gerlang}></div>
+          </div>
+        </div>
+        <div className={styles.description}>
+          <h1>Представляем работы мастера международного класса по рукоделию</h1>
+          <h2>Елены Белоусовой</h2>
+        </div>
       </div>
     </header>
   );
